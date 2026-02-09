9ea14d9fc8f27be4e67e04638ebe95f1a765f80e
Stian Pettersen <stianjp@hotmail.com>
2026-02-09 17:53:36 +0100

    chore(build): allow optional skip of typecheck

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: process.env.SKIP_TYPECHECK === "1",
+  },
 };
 
 export default nextConfig;
